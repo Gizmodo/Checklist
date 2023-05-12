9d141e94ab43807e7053c00898a80ff00956319e
SS <smirnovsergeyterem@gmail.com>
2023-05-12 13:22:17 +0300

    Zone add

--- app/src/main/java/ru/dl/checklist/app/presenter/MainActivity.kt
+++ app/src/main/java/ru/dl/checklist/app/presenter/MainActivity.kt
@@ -1,8 +1,6 @@
 package ru.dl.checklist.app.presenter
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -37,22 +35,6 @@ class MainActivity : AppCompatActivity() {
 
     fun setActionBarTitle(title: String) {
         binding.toolbar.title = title
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
